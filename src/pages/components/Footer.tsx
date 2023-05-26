ceee4fccba1c13bc9b230b243ed27fe7ac0985f5
yohanessilitonga <yohanesrfsilitonga21@gmail.com>
2023-05-26 20:18:41 +0800

    feat: adding header main and footer

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto py-4 text-center">
+      <span>@2023 Yohanes Silitonga</span>
+    </footer>
+  );
+};
+
+export default Footer;
